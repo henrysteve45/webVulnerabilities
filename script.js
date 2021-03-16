d140af9e0d4e400783d2b3c3f7028717fe6fd8c0
Steve Henry <henry.steve45@gmail.com>
2021-03-16 14:39:45 -0400

    more tests added

--- script.js
+++ script.js
@@ -48,14 +48,54 @@
     else {$("#browserDuck").append("<span class='text-danger'>Unknown browser</span>");}
 
 /*Plugins installed*/
-var x = navigator.plugins.length; 
-var txt = "<br>";
-if (x == 0) {
-    $("#plugins").append("<span class='text-danger'>No plugins available</span>");
-} else {
-    for(var i=0;i<x;i++)
-    {
-      txt+=navigator.plugins[i].name + "<br>"; 
+    var x = navigator.plugins.length; 
+    var txt = "<br>";
+    if (x == 0) {
+        $("#plugins").append("<span class='text-warning'>No plugins available or your plugins are hidden.</span>");
+    } else {
+        for(var i=0;i<x;i++)
+        {
+          txt+=navigator.plugins[i].name + "<br>"; 
+        }
+        $("#plugins").append(txt);
     }
-    $("#plugins").append(txt);
+
+/*CPU info*/
+    $("#cpu").append(navigator.hardwareConcurrency + " core(s)");
+
+/*Battery info*/
+try {
+    navigator.getBattery().then(function(battery) {
+        var level = Math.round(battery.level*100);
+        $("#battery").append("Your battery percentage is: " + level + "%.");
+        if (level == 100) {
+            $("#battery").append("<p>Your battery is fully charged!</p>");
+        } else if (level >= 75 && level <= 99) {
+            $("#battery").append("<p>Your device is almost fully charged!</p>");
+        } else if (level >= 30 && level < 75) {
+            $("#battery").append("<p>You will need to charge your device later.</p>");
+        } else if (level >= 15 && level < 30) {
+            $("#battery").append("<p>You need to charge your device.</p>");
+        } else {
+            $("#battery").append("<p>Your battery is critically low!</p>");
+        }
+    })
+} catch (error) {
+    console.error(error);
+    $("#battery").append("<p class='text-warning'>You are not using a battery or your browser does not support this feature.</p>");
 }
+
+/*IP address*/
+function text(url) {
+  return fetch(url).then(res => res.text());
+}
+
+text('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
+  let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/
+  let ip = data.match(ipRegex)[0];
+  $("#ip").append(ip);
+});
+
+/*connection*/
+$("#conSpeed").append(navigator.connection.downlink + " Mbits per second");
+$("#conType").append(navigator.connection.effectiveType);
