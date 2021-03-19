1c7e60483fad2274f581aea9c6e92a4b629e6dbd
Steve Henry <henry.steve45@gmail.com>
2021-03-19 12:31:35 -0400

    compass and location info

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*Operating System*/
     var OSName="Unknown OS";
     if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
-    if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+    if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS/iOS";
     if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
     if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux/Android";
     $("#os").append("<b>Operating System: </b>" + OSName + "<br>");
